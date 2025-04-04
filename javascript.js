dae51817668b8b73a013812f8b383777bae32831
Lovell-h <205021015+Lovell-h@users.noreply.github.com>
2025-04-04 10:57:36 -0500

    checking while

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
 function getcomputerchoice(){
     let x=0
+    console.log("before")
     while (x=0)
         {
             ifMath.floor(Math.random()*3);
             console.log(x);
         }
+    console.log("after" + x)
+    
     return x;
 }
