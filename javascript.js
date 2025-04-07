8e991b700bf5175a4ca0f27fe55f0fa9f4e2ccbe
Lovell-h <205021015+Lovell-h@users.noreply.github.com>
2025-04-07 05:57:31 -0500

    final version, replaced radio buttons with push buttons

--- javascript.js
+++ javascript.js
@@ -1,27 +1,33 @@
-let humanscore=0; /*ongoing player score */
-let computerscore=0;/*ongoing computer score*/
-let catscore=0; /*ongoing tie score */
-let rounds=0; /*current round number*/
-//function reload(){location.reload();} /*restart the game*/
+let humanscore=0; //ongoing player score 
+let computerscore=0; //ongoing computer score
+let catscore=0; //ongoing tie score 
+let rounds=0; //current round number
+
+
 const buttons = document.querySelectorAll("button");
 
 //listen for user interaction
 buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    alert(button.id) ;
-    if(button.id=="reset"{location.reload();})
+  button.addEventListener("click", () => 
+    {
+   if(button.id=="reload")location.reload();
+   playround(button.id);
+    })
+    
   });
-/*main function to play the game*/
-function playround()
+//main function to play the game
+function playround(userchoice)
 {
+    console.log("here");
     if (rounds>=5)
+        
         {
             alert("completed 5 rounds to play again please press reset");
         }else {
             
             rounds++;
             let comp = getcomputerchoice();
-            let user= getuserchoice(); 
+            let user= userchoice; 
             let winner=getresults(comp, user);
             let resultsmsg= "Round "+ rounds + "  of 5 results: <br>Human Choice: "
             resultsmsg=resultsmsg + user + "<br>Computer Choice: " + comp ;
@@ -36,13 +42,12 @@ function playround()
             resultsmsg=resultsmsg + " Computer: " +computerscore;
             resultsmsg=resultsmsg + " Ties: " +catscore;
                         
-            /*console.log (resultsmsg);*/
+            
             let x = "results["+rounds+"]";
-            /*console.log(x);*/
+            
             document.getElementById(x).innerHTML=resultsmsg;
             if(rounds==5)
                 {
-                    /*console.log("made it here");*/
                     fscore="Final Outcome: ";
                 if(humanscore>computerscore){
                     fscore= fscore + "Human wins...this time";
@@ -59,7 +64,7 @@ function playround()
         }
 }
 
-/* randomly choose rock, paper or scisor returns "compchoice" */
+// randomly choose rock, paper or scisor returns "compchoice" 
 function getcomputerchoice()
 {
     let myselection=Math.floor(Math.random() * 3) + 1;  
@@ -76,31 +81,10 @@ function getcomputerchoice()
             }
     return(compchoice);
 }
-/*get user input returns "userchoice" */
-function getuserchoice()
-{
-    let ele = document.getElementsByName('user-choice');
-    for(i=0;i<ele.length; i++)
-        {
-        if(ele[i].checked) selectedOption=ele[i].value; 
-    }
-        if(selectedOption==1)
-            {
-            userchoice="rock";
-        } else if (selectedOption==2)
-            {
-                userchoice="paper";
-            }
-            else if (selectedOption==3)
-            {
-                userchoice="scissors";
-            }
-            
-    return(userchoice);
-}
-/*determine the winner */
+
+//determine the winner 
 function getresults(comp2, user2){
-    /*console.log("computer:" + comp2 + " user: " +user2);*/
+  
     if(user2==comp2)
     {
         catscore++;
@@ -140,3 +124,29 @@ function getresults(comp2, user2){
                 }
             }
 }
+//retired code:
+/*
+// get user input returns "userchoice" 
+function getuserchoice()
+{
+    let ele = document.getElementsByName('user-choice');
+    for(i=0;i<ele.length; i++)
+        {
+        if(ele[i].checked) selectedOption=ele[i].value; 
+    }
+        if(selectedOption==1)
+            {
+            userchoice="rock";
+        } else if (selectedOption==2)
+            {
+                userchoice="paper";
+            }
+            else if (selectedOption==3)
+            {
+                userchoice="scissors";
+            }
+            
+    return(userchoice);
+}
+function reload(){location.reload();} //restart the game
+*/
